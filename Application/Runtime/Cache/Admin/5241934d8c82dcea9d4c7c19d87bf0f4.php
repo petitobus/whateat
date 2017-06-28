6a6fe1eb0ede407df3b61f424c6ff1cb4c871202
xuecan <yxcbellevie@gmail.com>
2017-06-29 01:04:46 +0200

    添加菜谱页面

--- Application/Runtime/Cache/Admin/5241934d8c82dcea9d4c7c19d87bf0f4.php
+++ Application/Runtime/Cache/Admin/5241934d8c82dcea9d4c7c19d87bf0f4.php
@@ -2,26 +2,55 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>添加城市</title>
+    <title>添加菜谱</title>
 </head>
 <body>
 <div>
-    <form action="post.shtml?method=create" method="post">
-        <table border="1">
-            <th colspan="4">添加城市</th>
-            <tr>
-                <td>城市名称</td>
-                <td><input type="text" required="required" name="name" placeholder="new city"></td>
-            </tr>
-            <tr>
-                <td>所属国家</td>
-                <td><input type="text" required="required" name="country" placeholder="new country"></td>
-            </tr>
-            <tr>
-                <td colspan="2"><input type="submit" value="         添加         " style="display: block;margin: 0 auto 0"></td>
-            </tr>
-        </table>
-    </form>
+    <table border="1">
+        <th colspan="4">添加菜谱</th>
+        <tr>
+            <td>菜谱名称</td>
+            <td><input type="text" required="required" id="name" placeholder="new city"></td>
+        </tr>
+        <tr>
+            <td>描述</td>
+            <td><input type="text" required="required" id="country" placeholder="new country"></td>
+        </tr>
+        <tr>
+            <td>步骤一</td>
+            <td><input type="text" required="required" id="step_1" placeholder="new country"></td>
+        </tr>
+        <tr>
+            <td>步骤二</td>
+            <td><input type="text" required="required" id="step_2" placeholder="new country"></td>
+        </tr>
+        <tr>
+            <td>食材一</td>
+            <td><input type="text" required="required" id="ingredient_1_name" placeholder="new country"></td>
+            <td><input type="text" required="required" id="ingredient_1_quantity" placeholder="new country"></td>
+            <td><input type="text" required="required" id="ingredient_1_level" placeholder="new country"></td>
+        </tr>        <tr>
+            <td>食材二</td>
+            <td><input type="text" required="required" id="ingredient_2_name" placeholder="new country"></td>
+            <td><input type="text" required="required" id="ingredient_2_quantity" placeholder="new country"></td>
+            <td><input type="text" required="required" id="ingredient_2_level" placeholder="new country"></td>
+        </tr>
+         <tr>
+            <td>调料一</td>
+            <td><input type="text" required="required" id="flavour_1_name" placeholder="new country"></td>
+            <td><input type="text" required="required" id="flavour_1_quantity" placeholder="new country"></td>
+            <td><input type="text" required="required" id="flavour_1_level" placeholder="new country"></td>
+        </tr>
+        <tr>
+            <td>食材2</td>
+            <td><input type="text" required="required" id="flavour_2_name" placeholder="new country"></td>
+            <td><input type="text" required="required" id="flavour_2_quantity" placeholder="new country"></td>
+            <td><input type="text" required="required" id="flavour_2_level" placeholder="new country"></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="         添加         " style="display: block;margin: 0 auto 0"></td>
+        </tr>
+    </table>
 </div>
 </body>
 </html>
